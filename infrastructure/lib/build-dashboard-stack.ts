fa4579123ca13c30c2d766630f81baac24960539
Kalalau Cantrell <cantrellkalalau@gmail.com>
2023-07-27 09:21:36 -0400

    Update name of build status topic

--- infrastructure/lib/build-dashboard-stack.ts
+++ infrastructure/lib/build-dashboard-stack.ts
@@ -81,7 +81,7 @@ export class BuildDashboardStack extends Stack {
       visibilityTimeout: Duration.seconds(60),
     });
     const topic = new sns.Topic(this, 'BuildStatusTopic', {
-      displayName: 'Build status',
+      displayName: 'Build Status Notifications',
     });
 
     const notificationLambda = new lambda.Function(this, "BuildNotificationFunction", {
